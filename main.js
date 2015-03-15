8720a3ffd0902af57d6212dc25228d6c34696cbe
CarlosRuizAscacibar <ruizcarlos93@yahoo.es>
2015-03-15 23:09:13 +0100

    Comented all the functions

--- main.js
+++ main.js
@@ -1,59 +1,60 @@
 // ==UserScript==
-// @name         Downloader
-// @namespace    http://your.homepage/
+// @name         BlackBoardDownloader
 // @version      0.1
-// @description  enter something useful
-// @author       You
+// @description  Creates a link next to every course. It downloads all the available contents of that subject
+// @author       Carlos Ruiz Ascacíbar
 // @match        https://unirioja.blackboard.com/webapps/portal/execute/tabs/tabAction?tab_tab_group_id=_1_1
 // @grant        none
 // ==/UserScript==
 IdModuloMisCursos = "module:_4_1";
 abiertas=0;
+/*
+Includes a JavaScript library injecting an element in the HTML document.
+*/
 function loadScript(url, callback)
 {
-    // Adding the script tag to the head as suggested before
     var head = document.getElementsByTagName('head')[0];
     var script = document.createElement('script');
     script.type = 'text/javascript';
     script.src = url;
-
-    // Then bind the event to the callback function.
-    // There are several events for cross browser compatibility.
     script.onreadystatechange = callback;
     script.onload = callback;
-
-    // Fire the loading
     head.appendChild(script);
 }
 loadScript('https://stuk.github.io/jszip/dist/jszip.js', function() {});
 loadScript('https://stuk.github.io/jszip/vendor/FileSaver.js', function() {console.log('librerias cargadas')});
 
 dp = new DOMParser();
+/*
+Adds the link next to the original course link.
+*/
 window.onkeypress = function(){
-  
-
 	var misCursos = document.getElementById(IdModuloMisCursos);
     misCursos = misCursos.getElementsByTagName('li');
-    //console.log(enlacesCursos);
     for(var i=0; i<misCursos.length ; i++){
         enlaceI = misCursos[i].getElementsByTagName('a')[0].getAttribute('href');
 		misCursos[i].innerHTML = misCursos[i].innerHTML + '<a id="asig'+i+'">Descargar contenido</a>';
 		document.getElementById('asig'+i).onclick=descargarAsignatura
 	}
-
-
 }
-function descargarAsignatura(enlaceAsignatura){
-		var enlace = enlaceAsignatura.currentTarget.parentNode.getElementsByTagName('a')[0].getAttribute('href');
-		var xhr = doAJAX(enlace);
-        doc = dp.parseFromString(xhr.response,"text/html");
-        contenido={};
-        contenido.nombre = enlaceAsignatura.currentTarget.parentNode.getElementsByTagName('a')[0].innerHTML;
-        contenido.path = "";
-        contenido.tipo = "carpeta";
-        contenido.subElementos = getEnlacesContenido(doc,"");
-        descargarContenido(contenido);
-		}
+/*
+Respons to the click event of the generated links.
+in: ev, Click event
+*/
+function descargarAsignatura(ev){
+	var enlace = ev.currentTarget.parentNode.getElementsByTagName('a')[0].getAttribute('href');
+	var xhr = doAJAX(enlace);
+    doc = dp.parseFromString(xhr.response,"text/html");
+    contenido={};
+    contenido.nombre = ev.currentTarget.parentNode.getElementsByTagName('a')[0].innerHTML;
+    contenido.path = "";
+    contenido.tipo = "carpeta";
+    contenido.subElementos = getEnlacesContenido(doc,"");
+    descargarContenido(contenido);
+}
+/*
+Creates and saves the zip
+*/
 function guardarZip(){
   zip = new JSZip();
   console.log(contenido);
@@ -61,26 +62,36 @@ function guardarZip(){
   generado = zip.generate({type:"blob"});
   saveAs(generado,contenido.nombre+".zip");
 }
+/*
+Preforms syncronous Ajax request
+in: enlace, string containig the refecence for the request
+out: returns xmlhttpRequest object containig the response of the request
+*/
 function doAJAX(enlace){
     var xhr = new XMLHttpRequest();
     xhr.open("GET",enlace,false);
     xhr.send();
-    //console.log(enlace , xhr);
     return xhr;
 }
+/*
+Recognises content type based on the img 
+in: contentListItem content to identify
+out: returns string with the type of the content
+*/
 function identificarContenido(contentListItem){
     var tipo = "n";
     if(contentListItem.getElementsByTagName("img")[0].getAttribute("src").search(/folder/i)>0)tipo="carpeta";
     if(contentListItem.getElementsByTagName("img")[0].getAttribute("src").search(/file/i)>0)tipo="fichero";
     return tipo;
 }
+/*
+Creates the object representing a content
+*/
 function getEnlacesContenido(doc,path){
     lista = doc.getElementById("content_listContainer");
     if(lista){
     var lis = lista.children;
         var arrayEnlaces = [] ;
-
-        //arryEnlaces.length = i;
         for(var i = 0 ; i<lis.length; i++)
         {
             arrayEnlaces[i]={};
@@ -97,28 +108,32 @@ function getEnlacesContenido(doc,path){
         return arrayEnlaces;
     }
 }
+/*
+Downloads all files
+*/
 function descargarContenido(nodo){
   for(var i = 0; i<nodo.subElementos.length;i++){
     if(nodo.subElementos[i].tipo === "fichero")doAJAXBlob(nodo.subElementos[i]);
     if(nodo.subElementos[i].tipo === "carpeta") descargarContenido(nodo.subElementos[i])
   }
 }
-   //doAJAXBlob(arrayEnlaces[i].enlace,arrayEnlaces[i],path,
-
+/*
+Adds al files to the zip
+*/
 function zipear(x,zip){
     //if(x){
     console.log(x);
     if( x.tipo === "carpeta" ){
          for(var i =0 ; i < x.subElementos.length; i++){
-             //console.log('carpeta ' + x.path);
              zipear(x.subElementos[i],zip)
          }
      }if( x.tipo === "fichero" ){
          zip.file(x.path + x.request.responseURL.substring(x.request.responseURL.lastIndexOf('/')+1) , x.request.response , {createFolders: true});
-         //console.log(x.path);
      }
-    //}
 }
+/*
+Does an asyncronous call for any kind of file
+*/
 function doAJAXBlob(file){
 var subxhr1 = new XMLHttpRequest();
 subxhr1.open('GET', file.enlace, true);
@@ -131,7 +146,6 @@ subxhr1.onreadystatechange = function (){
 		if(abiertas == 0)guardarZip();
 	}
 }
-//subxhr.onload = callback(subxhr,file,path);
 subxhr1.send();
 }
 
